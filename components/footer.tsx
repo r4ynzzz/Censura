06c3a64e33029df8b76e9b691e51ff0365aafd8c
r4ynzzz <ryanttncube@gmail.com>
2026-08-21 12:48:19 +0200

    Footer changes, just names

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,21 +3,8 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="w-full bg-black text-white">
-      <div className="max-h-6xl h-96 mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4">
+      <div className="max-h-6xl h-72 mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4">
         <div className="font-bold text-xl">Censura</div>
-
-        <div className="flex flex-col gap-2 text-sm">
-          <h1 className="font-bold text-xl">Company</h1>
-          <Link href="" className="hover:text-gray-600 transition-colors">
-            About
-          </Link>
-          <Link href="" className="hover:text-gray-600 transition-colors">
-            Contact
-          </Link>
-          <Link href="" className="hover:text-gray-600 transition-colors">
-            Support
-          </Link>
-        </div>
 
         <div className="flex flex-col gap-2 text-sm">
           <h1 className="font-bold text-xl">Features</h1>
@@ -41,7 +28,20 @@ export default function Footer() {
             Contact
           </Link>
           <Link href="" className="hover:text-gray-600 transition-colors">
+            Team
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-2 text-sm">
+          <h1 className="font-bold text-xl">More</h1>
+          <Link href="" className="hover:text-gray-600 transition-colors">
             Support
+          </Link>
+          <Link href="" className="hover:text-gray-600 transition-colors">
+            Reviews
+          </Link>
+          <Link href="" className="hover:text-gray-600 transition-colors">
+            Blogs
           </Link>
         </div>
 
