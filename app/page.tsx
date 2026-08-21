2b70969315d73833374db3a39929a39d5ca860f6
r4ynzzz <ryanttncube@gmail.com>
2026-08-21 11:31:18 +0200

    nav menu feature changes. added Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/footer";
+import Hero from "@/components/hero";
 import NavBar from "@/components/nav-bar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Hero />
       <Footer />
     </>
   );
