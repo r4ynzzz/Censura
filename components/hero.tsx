12e1ed8b8ca1c130b28a427d571811b7648ccf62
r4ynzzz <ryanttncube@gmail.com>
2026-08-24 14:40:53 +0200

    Added hero image. Spacing on Hero headers.

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,16 +1,29 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <div className="min-h-screen w-full bg-white flex flex-row m-4">
       {/* Info */}
       <div className="flex justify-center items-center w-1/2">
-        <div className="w-64">
-          <h1 className="font-bold text-5xl">Hero</h1>
-          <p className="">More info on more info. Get your feedback.</p>
+        <div className="w-13/15">
+          <h1 className="font-bold text-4xl pb-4">Empowering Businesses</h1>
+          <h1 className="font-bold text-4xl pb-4">With Effortless Feedback.</h1>
+          <p className="">
+            More info on more info. Get your feedback. there should be more
+            feedback info here. It should get even longer for crazier reasons.
+          </p>
         </div>
       </div>
 
       {/* image */}
-      <div className="flex w-1/2 bg-blue-900"></div>
+      <div className="flex w-1/2">
+        <Image
+          src="feedback_hero_two_phones_v10.svg"
+          width={500}
+          height={500}
+          alt="Feedback image"
+        />
+      </div>
     </div>
   );
 }
