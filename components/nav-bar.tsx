245390aad43d52146f6684baf864e8af377a1841
r4ynzzz <ryanttncube@gmail.com>
2026-08-26 18:52:41 +0200

    Nav color, opacity and structure changes

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -67,8 +67,8 @@ export default function NavBar() {
   const router = useRouter();
 
   return (
-    <nav className="bg-blue-900 text-white drop-shadow-md sticky top-0 z-1">
-      <div className="flex w-full items-center justify-between py-4 px-6 lg:px-14">
+    <nav className="flex bg-blue-900/99 text-white drop-shadow-md justify-center items-center sticky top-0 z-1">
+      <div className="bg-blue-950/60 flex items-center justify-between rounded-4xl py-4 px-6 lg:px-14 w-3/4 m-2">
         <h1 className="font-bold">Censura</h1>
         <span>
           <NavigationMenu>
