98e8675e05dde4144918546f26ebc2658cd2985b
r4ynzzz <ryanttncube@gmail.com>
2026-09-01 11:32:40 +0200

    Feature Card structure changes

--- components/features.tsx
+++ components/features.tsx
@@ -25,59 +25,67 @@ export default function Features() {
         </h1>
       </div>
       <div className="flex items-center justify-between w-3/4">
-        <Card className="w-80 h-80 rounded-2xl">
-          <CardHeader className="h-36">
-            <CardTitle>Custom Forms</CardTitle>
-            <CardDescription>
-              Create forms with questions regarding anything. Forms come in many
-              different structures.
-            </CardDescription>
-            <CardAction>Action</CardAction>
-          </CardHeader>
-          <CardContent>
-            <p>Content</p>
+        <Card className="flex justify-start w-80 h-96 rounded-2xl">
+          <CardContent className="m-2">
+            <CardContent className="flex">
+              <Image
+                src="/FTWDMAC.jpg"
+                width={250}
+                height={250}
+                alt="Fleetwood Mac"
+                className="rounded"
+              />
+            </CardContent>
+            <CardContent className="flex-col items-start mt-4">
+              <CardTitle className="font-extrabold">Fleetwood Mac</CardTitle>
+              <CardDescription className="mt-3">
+                Only the best band to ever exist. Cannot be topped by anyone. Go
+                ahead, try.
+              </CardDescription>
+            </CardContent>
           </CardContent>
-          <CardFooter>
-            <p>Footer</p>
-          </CardFooter>
         </Card>
-        <Card className="w-80 h-80 rounded-2xl">
-          <CardHeader className="h-36">
-            <CardTitle>Form Analytics</CardTitle>
-            <CardDescription>
-              Results from forms are analyzed. Prominent data is displayed to be
-              used.
-            </CardDescription>
-            <CardAction>Action</CardAction>
-          </CardHeader>
-          <CardContent>
-            <p>Content</p>
+
+        <Card className="flex justify-start w-80 h-96 rounded-2xl">
+          <CardContent className="m-2">
+            <CardContent className="flex">
+              <Image
+                src="/FTWDMAC.jpg"
+                width={250}
+                height={250}
+                alt="Fleetwood Mac"
+                className="rounded"
+              />
+            </CardContent>
+            <CardContent className="flex-col items-start mt-4">
+              <CardTitle className="font-extrabold">Fleetwood Mac</CardTitle>
+              <CardDescription className="mt-3">
+                Only the best band to ever exist. Cannot be topped by anyone. Go
+                ahead, try.
+              </CardDescription>
+            </CardContent>
           </CardContent>
-          <CardFooter>
-            <p>Footer</p>
-          </CardFooter>
         </Card>
-        <Card className="w-80 h-96 rounded-2xl">
-          <CardHeader className="h-10">
-            <CardTitle>Fleetwood Mac</CardTitle>
-            <CardAction>Action</CardAction>
-          </CardHeader>
-          <CardContent className="flex items-center justify-center">
-            <Image
-              src="/FTWDMAC.jpg"
-              width={250}
-              height={250}
-              alt="Fleetwood Mac"
-              className="rounded"
-            />
+
+        <Card className="flex justify-start w-80 h-96 rounded-2xl">
+          <CardContent className="m-2">
+            <CardContent className="flex">
+              <Image
+                src="/FTWDMAC.jpg"
+                width={250}
+                height={250}
+                alt="Fleetwood Mac"
+                className="rounded"
+              />
+            </CardContent>
+            <CardContent className="flex-col items-start mt-4">
+              <CardTitle className="font-extrabold">Fleetwood Mac</CardTitle>
+              <CardDescription className="mt-3">
+                Only the best band to ever exist. Cannot be topped by anyone. Go
+                ahead, try.
+              </CardDescription>
+            </CardContent>
           </CardContent>
-          <CardFooter className="flex-col items-start">
-            <p>Footer</p>
-            <CardDescription>
-              Only the best band to ever exist. Cannot be topped by anyone. Go
-              ahead, try.
-            </CardDescription>
-          </CardFooter>
         </Card>
       </div>
     </div>
--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-black text-white">
+    <footer className="w-full bg-blue-900 text-white">
       <div className="max-h-6xl h-72 mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4">
-        <div className="font-bold text-xl">Censura</div>
+        <div className="font-bold text-xl text-sky-400">Censura</div>
 
         <div className="flex flex-col gap-2 text-sm">
           <h1 className="font-bold text-xl">Features</h1>
